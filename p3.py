849d60668b71cc0f0d771e50c9c4f40dc0950283
Alex Cimbron <u1994959@campus.udg.edu>
2026-04-15 13:03:38 +0200

    primer

--- p3.py
+++ p3.py
@@ -1,6 +1,14 @@
 # Codi de la prac tres
-import scikit-learn
-from sklearn import datasets
-from sklearn import svm
-from sklearn import metrics
+
+#Importem les llibreries que ens diu el professor
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.dummy  import DummyClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Creem el data seed
+df =  pd.read_csv("star_wars_charecter_dataset.csv")
+
+#Agafem variables immportants
 
